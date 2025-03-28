fc2e42c2659a04109ea091c98ef2cbf1c491ec15
Pranjali Sachan <pranjaligtm123@gmail.com>
2025-03-28 18:22:36 -0400

    removed proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://parking-backend-t2sv.onrender.com", // Proxy API requests to Django backend
-    },
-  },
+  plugins: [react()]
 })
